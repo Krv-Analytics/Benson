6313b2f612db98541f868c2cc6e306c80811a3c6
jeremy.wayland <jeremy.don.wayland@gmail.com>
2025-03-26 16:04:43 -0700

    add `PreprocessingConfig` and PydanticV2 practices for config types

--- benson/__init__.py
+++ benson/__init__.py
@@ -1,30 +1,9 @@
-from pydantic import BaseModel, Field
+from pydantic import BaseModel, Field, ConfigDict
 from typing import Dict, List, Any
-from itertools import product
+from typing import ClassVar
 from sklearn.model_selection import ParameterGrid
 
 class ImputationGrid(BaseModel):
-    """
-    Represents a grid of parameters for hyperparameter tuning.
-
-    This class is designed to facilitate the organization and management of hyperparameter grids 
-    for different machine learning models. It uses `ParameterGrid` from scikit-learn to define 
-    parameter combinations and provides a structure to associate these grids with specific models.
-
-    Attributes:
-        methods (List[str]): 
-            A list of strings representing sklearn-compatible model names. The index of each model 
-            corresponds to the index of its associated parameter grid in the `grids` attribute and 
-            its module in the `modules` attribute.
-        grids (List[ParameterGrid]): 
-            A list of `ParameterGrid` objects containing hyperparameter combinations for different models. 
-            The index of each grid corresponds to the index of its associated model in the `methods` 
-            attribute and its module in the `modules` attribute.
-        modules (List[str]): 
-            A list of strings representing the names of sklearn modules that can be loaded dynamically 
-            to access the corresponding models. The index of each module corresponds to the index of 
-            its associated model in the `methods` attribute and its parameter grid in the `grids` attribute.
-    """
     methods: List[str] = Field(
         default_factory=list,
         description="A list of sklearn model names, where the index maps to the corresponding ParameterGrid and module."
@@ -38,5 +17,23 @@ class ImputationGrid(BaseModel):
         description="A list of ParameterGrid objects, where the index maps to the corresponding model and module."
     )
 
-    class Config:
-        arbitrary_types_allowed = True
+    # Annotate Config as ClassVar
+    Config: ClassVar[ConfigDict] = ConfigDict(arbitrary_types_allowed=True)
+
+        
+class PreprocessingConfig(BaseModel):
+    method: str = Field(
+        default="StandardScaler",
+        description="The name of the sklearn model for which the preprocessing configuration is defined."
+    )
+    module: str = Field(
+        default="sklearn.preprocessing",
+        description="The name of the sklearn module that contains the specified model."
+    )
+    params: Dict = Field(
+        default={},
+        description="A parameter configuration, where keys are parameters for the method and values are the required value."
+    )
+
+    # Annotate Config as ClassVar
+    Config: ClassVar[ConfigDict] = ConfigDict(arbitrary_types_allowed=True)
